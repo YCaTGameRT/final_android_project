22815158a0120fe74168a1e546c94a2387362268
YCaTGameRT <ycatgamert@gmail.com>
2026-06-09 15:33:16 +0500

    notes added

---
+++ app/src/main/java/com/ycatgamert/finalproject/DatabaseHelper.java
@@ -0,0 +1,62 @@
+package com.ycatgamert.finalproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "cat_tools_notes.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "notes";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_TEXT = "note_text";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TEXT + " TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addNote(String noteText) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_TEXT, noteText);
+        long result = db.insert(TABLE_NAME, null, cv);
+        return result != -1;
+    }
+
+    public ArrayList<String> getAllNotes() {
+        ArrayList<String> notesList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                notesList.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return notesList;
+    }
+
+    public void deleteNote(String noteText) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME, COLUMN_TEXT + "=?", new String[]{noteText});
+    }
+}
---
+++ app/src/main/java/com/ycatgamert/finalproject/NotesActivity.java
@@ -0,0 +1,62 @@
+package com.ycatgamert.finalproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class NotesActivity extends AppCompatActivity {
+
+    private ArrayList<String> notesList;
+    private ArrayAdapter<String> adapter;
+    private EditText noteInput;
+    private DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+
+        noteInput = findViewById(R.id.noteInput);
+        Button buttonAdd = findViewById(R.id.buttonAdd);
+        ListView notesListView = findViewById(R.id.notesListView);
+
+        dbHelper = new DatabaseHelper(this);
+        notesList = dbHelper.getAllNotes();
+
+        adapter = new ArrayAdapter<>(this, R.layout.list_item_note, R.id.noteText, notesList);
+        notesListView.setAdapter(adapter);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = noteInput.getText().toString().trim();
+                if (!text.isEmpty()) {
+                    boolean inserted = dbHelper.addNote(text);
+                    if (inserted) {
+                        notesList.add(text);
+                        adapter.notifyDataSetChanged();
+                        noteInput.setText("");
+                    } else {
+                        Toast.makeText(NotesActivity.this, "Ошибка!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        notesListView.setOnItemLongClickListener((parent, view, position, id) -> {
+            String selectedNote = notesList.get(position);
+            dbHelper.deleteNote(selectedNote);
+            notesList.remove(position);
+            adapter.notifyDataSetChanged();
+            Toast.makeText(NotesActivity.this, "Удалено", Toast.LENGTH_SHORT).show();
+            return true;
+        });
+    }
+}
+
